1ea930c7b7819768414ff9926bbb09e71142b054
chinadarren <chinadarren@outlook.com>
2015-08-12 00:47:59 +0800

    s

--- src/com/example/SMSTest/MyActivity.java
+++ src/com/example/SMSTest/MyActivity.java
@@ -1,56 +1,100 @@
 package com.example.SMSTest;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Objects;
 
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-   // private static final String ACTIVITY_TAG="LogDemo";
+ //   private static final String ACTIVITY_TAG="LogDemo";
     private TextView sender;
-
     private TextView content;
     private IntentFilter receiveFileter;
     private MessageReceiver messageReciver;
+
+    private EditText to;
+    private EditText msgInput;
+    private Button send;
+
+    private IntentFilter sendFilter;
+    private SendStatusReceiver sendStatusReceiver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-      //  sender = (TextView) findViewById(R.id.sender);
+
+        sender = (TextView) findViewById(R.id.sender);
         content = (TextView) findViewById(R.id.content);
         receiveFileter = new IntentFilter();
-        receiveFileter.addAction("adnroid.provider.Telephony.SMS_RECEIVED");
+        receiveFileter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        receiveFileter.setPriority(100);
         messageReciver = new MessageReceiver();
         registerReceiver(messageReciver, receiveFileter);
       //  Log.d(MyActivity.ACTIVITY_TAG, "this is log1");
+
+        to = (EditText) findViewById(R.id.to);
+        msgInput = (EditText) findViewById(R.id.msg_input);
+        send = (Button) findViewById(R.id.send);
+
+        sendFilter = new IntentFilter();
+        sendFilter.addAction("SENT_SMS_ACTION");
+        sendStatusReceiver = new SendStatusReceiver();
+        registerReceiver(sendStatusReceiver,sendFilter);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager smsManager = SmsManager.getDefault();
+
+                Intent sentIntent = new Intent("SENT_SMS_ACTION");
+                PendingIntent pi = PendingIntent.getBroadcast(MyActivity.this,0,sentIntent,0);
+                smsManager.sendTextMessage(to.getText().toString(),null,msgInput.getText().toString(),pi,null);
+
+                smsManager.sendTextMessage(to.getText().toString(),null,
+                        msgInput.getText().toString(),null,null);
+            }
+        });
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
         unregisterReceiver(messageReciver);
-      //  Log.d(MyActivity.ACTIVITY_TAG, "this is log2");
+        unregisterReceiver(sendStatusReceiver);
+     //   Log.d(MyActivity.ACTIVITY_TAG, "this is log2");
+    }
+    class SendStatusReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(getResultCode() == RESULT_OK ){
+                //短信发送成功
+                Toast.makeText(context,"Send succeeded",Toast.LENGTH_SHORT).show();
+            }else {
+                //短信发送失败
+                Toast.makeText(context,"Send failed",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     class MessageReceiver extends BroadcastReceiver{
         @Override
         public void onReceive(Context context, Intent intent) {
          //   Log.d(MyActivity.ACTIVITY_TAG,"this is log3");
-            sender = (TextView) findViewById(R.id.sender);
             Bundle bundle = intent.getExtras();
             //提取短信消息
             // pdu密钥来提取 一个 SMS pdus 数组
@@ -68,8 +112,9 @@ public class MyActivity extends Activity {
                 //调用getMessageBody方法获取短信内容
                 fullMessage += message.getMessageBody();
             }
-            sender.setText("sss");
+            sender.setText(address);
             content.setText(fullMessage);
+            abortBroadcast();
         }
     }
 }
