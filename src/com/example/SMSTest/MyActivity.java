b47171d6c97388163e318bd50ee54d6c97a202b4
chinadarren <chinadarren@outlook.com>
2015-08-12 10:55:19 +0800

    s

--- src/com/example/SMSTest/MyActivity.java
+++ src/com/example/SMSTest/MyActivity.java
@@ -29,9 +29,9 @@ public class MyActivity extends Activity {
     private EditText to;
     private EditText msgInput;
     private Button send;
-
     private IntentFilter sendFilter;
     private SendStatusReceiver sendStatusReceiver;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,7 +45,7 @@ public class MyActivity extends Activity {
         receiveFileter.setPriority(100);
         messageReciver = new MessageReceiver();
         registerReceiver(messageReciver, receiveFileter);
-        //  Log.d(MyActivity.ACTIVITY_TAG, "this is log1");
+        //       Log.d(MyActivity.ACTIVITY_TAG, "this is log1");
 
         to = (EditText) findViewById(R.id.to);
         msgInput = (EditText) findViewById(R.id.msg_input);
@@ -54,7 +54,7 @@ public class MyActivity extends Activity {
         sendFilter = new IntentFilter();
         sendFilter.addAction("SENT_SMS_ACTION");
         sendStatusReceiver = new SendStatusReceiver();
-        registerReceiver(sendStatusReceiver,sendFilter);
+        registerReceiver(sendStatusReceiver, sendFilter);
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,36 +62,37 @@ public class MyActivity extends Activity {
                 SmsManager smsManager = SmsManager.getDefault();
 
                 Intent sentIntent = new Intent("SENT_SMS_ACTION");
-                PendingIntent pi = PendingIntent.getBroadcast(MyActivity.this,0,sentIntent,0);
-                smsManager.sendTextMessage(to.getText().toString(),null,msgInput.getText().toString(),pi,null);
+                PendingIntent pi = PendingIntent.getBroadcast(MyActivity.this, 0, sentIntent, 0);
+                smsManager.sendTextMessage(to.getText().toString(), null, msgInput.getText().toString(), pi, null);
 
-                smsManager.sendTextMessage(to.getText().toString(),null,
-                        msgInput.getText().toString(),null,null);
+                smsManager.sendTextMessage(to.getText().toString(), null,
+                        msgInput.getText().toString(), null, null);
             }
         });
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(messageReciver);
         unregisterReceiver(sendStatusReceiver);
         //   Log.d(MyActivity.ACTIVITY_TAG, "this is log2");
     }
-    class SendStatusReceiver extends BroadcastReceiver{
+
+    class SendStatusReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if(getResultCode() == RESULT_OK ){
+            if (getResultCode() == RESULT_OK) {
                 //短信发送成功
-                Toast.makeText(context,"Send succeeded",Toast.LENGTH_SHORT).show();
-            }else {
+                Toast.makeText(context, "Send succeeded", Toast.LENGTH_SHORT).show();
+            } else {
                 //短信发送失败
-                Toast.makeText(context,"Send failed",Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "Send failed", Toast.LENGTH_LONG).show();
             }
         }
     }
 
-    class MessageReceiver extends BroadcastReceiver{
+    class MessageReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             //   Log.d(MyActivity.ACTIVITY_TAG,"this is log3");
@@ -100,15 +101,15 @@ public class MyActivity extends Activity {
             // pdu密钥来提取 一个 SMS pdus 数组
             Object[] pdus = (Object[]) bundle.get("pdus");
             SmsMessage[] messages = new SmsMessage[pdus.length];
-            for(int i = 0 ; i<messages.length;i++){
+            for (int i = 0; i < messages.length; i++) {
                 //createFromPdu方法将每一个pdu字节数组转换为SmsMessage对象
-                messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
             }
             //调用getOriginatingAddress方法获取发送方号码
             String address = messages[0].getOriginatingAddress();
             String fullMessage = "";
 
-            for (SmsMessage message : messages){
+            for (SmsMessage message : messages) {
                 //调用getMessageBody方法获取短信内容
                 fullMessage += message.getMessageBody();
             }
